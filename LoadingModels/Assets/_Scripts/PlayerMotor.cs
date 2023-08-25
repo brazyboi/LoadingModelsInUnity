c1aa16aa87b2643553397f5a4ee8c0290441546c
brazyboi <zhonglucas@gmail.com>
2023-08-25 11:17:39 -0700

    Fix gravity of player

--- LoadingModels/Assets/_Scripts/PlayerMotor.cs
+++ LoadingModels/Assets/_Scripts/PlayerMotor.cs
@@ -6,7 +6,9 @@ public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    private bool isGrounded;
     public float speed = 5f;
+    public float gravity = -9.8f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,7 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = controller.isGrounded;
     }
     // Receives inputs from input manager script
     public void ProcessMove(Vector2 input)
@@ -25,5 +27,11 @@ public class PlayerMotor : MonoBehaviour
         moveDirection.x = input.x;
         moveDirection.z = input.y;  
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime; 
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
